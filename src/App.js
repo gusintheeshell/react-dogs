f4bd7414bfcd0695e7b09676302a6457f46b8331
Gus <gusbdev@gmail.com>
2021-08-29 19:33:15 -0300

    Applying context

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import Home from "./Components/Home/Home";
 import Login from "./Components/Login/Login";
+import { UserStorage } from "./Contexts/UserContext";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Header />
-        <Route>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" component={Login} />
-        </Route>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Route>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" component={Login} />
+          </Route>
+          <Footer />
+        </UserStorage>
       </Router>
     </div>
   );

523708a54d93eb15380bb6e798b3c3b573203fc6
Gus <gusbdev@gmail.com>
2021-08-29 17:27:13 -0300

    Components for Login, Button, Input

---
+++ src/Components/Login/LoginForm/LoginForm.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Button from "../../Forms/Button/Button";
+import Input from "../../Forms/Input/Input";
+
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+      });
+  }
+
+  return (
+    <section>
+      <h1>Login</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" />
+        <Input label="Senha" type="password" name="password" />
+        <Button>Entrar</Button>
+      </form>
+      <Link to="/login/create">Cadastros</Link>
+    </section>
+  );
+};
+
+export default LoginForm;

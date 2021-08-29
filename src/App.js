f093a7f6d5f37b8a8c083738906486a04ad0d96a
Gus <gusbdev@gmail.com>
2021-08-29 13:07:49 -0300

    Config routes, initial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;

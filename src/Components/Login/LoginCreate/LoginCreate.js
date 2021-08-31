f56dddfb67e3fa2d8a7cc88fed377f14d4f87766
Gus <gusbdev@gmail.com>
2021-08-30 22:11:22 -0300

    Implement useFetch

--- src/Components/Login/LoginCreate/LoginCreate.js
+++ src/Components/Login/LoginCreate/LoginCreate.js
@@ -1,9 +1,11 @@
 import React, { useContext } from "react";
 import Button from "../../Forms/Button/Button";
 import Input from "../../Forms/Input/Input";
+import Error from "../../../Helper/Error";
 import useForm from "../../../Hooks/useForm";
 import { UserContext } from "../../../Contexts/UserContext";
 import { USER_POST } from "../../../Services/api";
+import useFetch from "../../../Hooks/useFetch";
 
 const LoginCreate = () => {
   const username = useForm();
@@ -11,6 +13,7 @@ const LoginCreate = () => {
   const password = useForm();
 
   const { userLogin } = useContext(UserContext);
+  const { loading, error, request } = useFetch();
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -19,9 +22,8 @@ const LoginCreate = () => {
       email: email.value,
       password: password.value,
     });
-    const response = await fetch(url, options);
+    const { response } = await request(url, options);
     if (response.ok) userLogin(username.value, password.value);
-    console.log(response);
   }
   return (
     <section className="animeLeft">
@@ -30,7 +32,12 @@ const LoginCreate = () => {
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Email" type="email" name="email" {...email} />
         <Input label="Senha" type="password" name="password" {...password} />
-        <Button>Cadastrar</Button>
+        {loading ? (
+          <Button disable>Cadastrando...</Button>
+        ) : (
+          <Button>Cadastrar</Button>
+        )}
+        <Error error={error} />
       </form>
     </section>
   );

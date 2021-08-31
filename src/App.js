85ab6d2f477ca3147abafa0339ddbf9d7884c3d0
Gus <gusbdev@gmail.com>
2021-08-30 22:28:56 -0300

    Implement ProtectedRoute component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Footer from "./Components/Footer/Footer";
 import Home from "./Components/Home/Home";
 import Login from "./Components/Login/Login";
 import { UserStorage } from "./Contexts/UserContext";
+import User from "./Components/User/User";
+import ProtectedRoute from "./Helper/Route/ProtectedRoute";
 
 function App() {
   return (
@@ -15,7 +17,8 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <ProtectedRoute path="conta/*" element={<User />} />
           </Routes>
           <Footer />
         </UserStorage>

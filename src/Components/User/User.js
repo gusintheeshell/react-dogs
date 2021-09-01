3156cff9008372b3b70d6e39387db738e4ac31c4
Gus <gusbdev@gmail.com>
2021-08-31 22:36:18 -0300

    User component routes

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Feed from "../Feed/Feed";
+import UserHeader from "./UserHeader/UserHeader";
+import UserPhotoPost from "./UserHeader/UserPhotoPost";
+import UserStats from "./UserHeader/UserStats";
 
 const User = () => {
-  return <div>User</div>;
+  return (
+    <section className="container">
+      <UserHeader />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/post" element={<UserPhotoPost />} />
+        <Route path="/stats" element={<UserStats />} />
+      </Routes>
+    </section>
+  );
 };
 
 export default User;

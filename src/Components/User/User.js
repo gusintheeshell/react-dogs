a6dfdcdc0837c018ffe8dfa5969aa2542fdd996b
Gus <gusbdev@gmail.com>
2021-09-02 22:27:54 -0300

    Refactor file folders

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Feed from "../Feed/Feed";
 import UserHeader from "./UserHeader/UserHeader";
-import UserPhotoPost from "./UserHeader/UserPhotoPost";
-import UserStats from "./UserHeader/UserStats";
+import UserPhotoPost from "./UserPhotoPost/UserPhotoPost";
+import UserStats from "./UserStats/UserStats";
 
 const User = () => {
   return (

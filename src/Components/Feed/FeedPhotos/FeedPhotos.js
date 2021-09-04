7efd82565d429583cee983b71a43ee63d06b9775
Gus <gusbdev@gmail.com>
2021-09-04 13:19:21 -0300

    Initial Feed Photos component

---
+++ src/Components/Feed/FeedPhotos/FeedPhotos.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import FeedPhotoItems from "../FeedPhotoItems/FeedPhotoItems";
+import Error from "../../../Helper/Error/Error";
+import Loading from "../../../Helper/Loading/Loading";
+import useFetch from "../../../Hooks/useFetch";
+import { PHOTOS_GET } from "../../../Services/api";
+import styles from "./FeedPhotos.module.css";
+
+const FeedPhoto = () => {
+  const { data, loading, error, request } = useFetch();
+
+  useEffect(() => {
+    async function fetchPhotos() {
+      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+      const { json, response } = await request(url, options);
+      console.log(json);
+    }
+    fetchPhotos();
+  }, [request]);
+
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <ul className={`${styles.feed} animeLeft`}>
+        {data.map((photo) => (
+          <FeedPhotoItems key={photo.id} photo={photo} />
+        ))}
+      </ul>
+    );
+  else return null;
+};
+
+export default FeedPhoto;

8934c1a1abb80496a0bd952759c0829017bbfeaf
Gus <gusbdev@gmail.com>
2021-09-04 15:07:31 -0300

    FeedPhotoItems Initial component

--- src/Components/Feed/FeedPhotoItems/FeedPhotoItems.js
+++ src/Components/Feed/FeedPhotoItems/FeedPhotoItems.js
@@ -1,9 +1,13 @@
 import React from "react";
 import styles from "./FeedPhotoItems.module.css";
 
-const FeedPhotoItems = ({ photo }) => {
+const FeedPhotoItems = ({ photo, setModalPhoto }) => {
+  function handleClick() {
+    setModalPhoto(photo);
+  }
+
   return (
-    <li className={styles.photo}>
+    <li className={styles.photo} onClick={handleClick}>
       <img src={photo.src} alt={photo.title} />
       <span className={styles.visualizacao}>{photo.acessos}</span>
     </li>

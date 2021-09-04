888a0537002e30f49ea0836daf833f73bde839c8
Gus <gusbdev@gmail.com>
2021-09-04 15:06:57 -0300

    Feed modal initial

--- src/Components/Feed/FeedModal/FeedModal.js
+++ src/Components/Feed/FeedModal/FeedModal.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Error from "../../../Helper/Error/Error";
+import Loading from "../../../Helper/Loading/Loading";
+import useFetch from "../../../Hooks/useFetch";
+import { PHOTO_GET } from "../../../Services/api";
+import PhotoContent from "../../Photo/PhotoContent/PhotoContent";
+import styles from "./FeedModal.module.css";
 
-const FeedModal = () => {
-  return <div>Feed Modal</div>;
+const FeedModal = ({ photo, setModalPhoto }) => {
+  const { data, error, loading, request } = useFetch();
+
+  useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+
+  function handleOutsideClick(event) {
+    if (event.target === event.currentTarget) setModalPhoto(null);
+  }
+
+  return (
+    <div className={styles.modal} onClick={handleOutsideClick}>
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </div>
+  );
 };
 
 export default FeedModal;

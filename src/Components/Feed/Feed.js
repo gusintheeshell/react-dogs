61895cec95c9578408ec730c8b8342d6dc81a226
Gus <gusbdev@gmail.com>
2021-09-04 13:16:52 -0300

    Inserting components

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,7 +1,14 @@
 import React from "react";
+import FeedModal from "./FeedModal/FeedModal";
+import FeedPhoto from "./FeedPhotos/FeedPhotos";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <div>
+      <FeedModal />
+      <FeedPhoto />
+    </div>
+  );
 };
 
 export default Feed;

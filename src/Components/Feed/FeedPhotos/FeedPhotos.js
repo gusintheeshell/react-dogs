77f59d5ed9485f09f60279eae9560134f317c735
Gus <gusbdev@gmail.com>
2021-09-04 15:08:01 -0300

    FeedPhotos component

--- src/Components/Feed/FeedPhotos/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos/FeedPhotos.js
@@ -6,7 +6,7 @@ import useFetch from "../../../Hooks/useFetch";
 import { PHOTOS_GET } from "../../../Services/api";
 import styles from "./FeedPhotos.module.css";
 
-const FeedPhoto = () => {
+const FeedPhotos = ({ setModalPhoto }) => {
   const { data, loading, error, request } = useFetch();
 
   useEffect(() => {
@@ -24,11 +24,15 @@ const FeedPhoto = () => {
     return (
       <ul className={`${styles.feed} animeLeft`}>
         {data.map((photo) => (
-          <FeedPhotoItems key={photo.id} photo={photo} />
+          <FeedPhotoItems
+            key={photo.id}
+            photo={photo}
+            setModalPhoto={setModalPhoto}
+          />
         ))}
       </ul>
     );
   else return null;
 };
 
-export default FeedPhoto;
+export default FeedPhotos;

44a782de52815356d798f18ee80bbe68d3c16113
Gus <gusbdev@gmail.com>
2021-09-08 22:27:31 -0300

    Photo delete component

---
+++ src/Components/Photo/PhotoDelete/PhotoDelete.js
@@ -0,0 +1,34 @@
+import React from "react";
+import useFetch from "../../../Hooks/useFetch";
+import { PHOTO_DELETE } from "../../../Services/api";
+import styles from "./PhotoDelete.module.css";
+
+const PhotoDelete = ({ id }) => {
+  const { loading, request } = useFetch();
+
+  async function handleClick(event) {
+    const confirm = window.confirm("Tem certeza que deseja deletar?");
+    if (confirm) {
+      const token = window.localStorage.getItem("token");
+      const { url, options } = PHOTO_DELETE(token, id);
+      const { response } = await request(url, options);
+      if (response.ok) window.location.reload();
+    }
+  }
+
+  return (
+    <>
+      {loading ? (
+        <button className={styles.delete} disabled>
+          Deletar
+        </button>
+      ) : (
+        <button onClick={handleClick} className={styles.delete}>
+          Deletar
+        </button>
+      )}
+    </>
+  );
+};
+
+export default PhotoDelete;

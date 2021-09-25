1b5f70bb0760cef87473a5090f8b8b00d642275e
Gus <gusbdev@gmail.com>
2021-09-25 13:47:55 -0300

    Photo route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./Components/Header/Header";
-import Footer from "./Components/Footer/Footer";
-import Home from "./Components/Home/Home";
-import Login from "./Components/Login/Login";
-import { UserStorage } from "./Contexts/UserContext";
-import User from "./Components/User/User";
-import ProtectedRoute from "./Helper/Route/ProtectedRoute";
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import { UserStorage } from './Contexts/UserContext';
+import User from './Components/User/User';
+import ProtectedRoute from './Helper/Route/ProtectedRoute';
+import Photo from './Components/Photo/Photo';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="login/*" element={<Login />} />
+            <Route path="photo/:id" element={<Photo />} />
             <ProtectedRoute path="account/*" element={<User />} />
           </Routes>
           <Footer />

4c769e2a13dc15d2a9e144ddcd89fe8b069ec0f4
Gus <gusbdev@gmail.com>
2021-09-26 15:01:43 -0300

    Change navigate

--- src/Contexts/UserContext.js
+++ src/Contexts/UserContext.js
@@ -1,6 +1,6 @@
-import React, { createContext, useCallback, useEffect, useState } from "react";
-import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET } from "../Services/api";
-import { useNavigate } from "react-router-dom";
+import React, { createContext, useCallback, useEffect, useState } from 'react';
+import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET } from '../Services/api';
+import { useNavigate } from 'react-router-dom';
 
 export const UserContext = createContext();
 
@@ -17,8 +17,8 @@ export const UserStorage = ({ children }) => {
       setError(null);
       setLoading(false);
       setLogin(false);
-      window.localStorage.removeItem("token");
-      navigate("/login");
+      window.localStorage.removeItem('token');
+      navigate('/login');
     },
     [navigate]
   );
@@ -39,9 +39,9 @@ export const UserStorage = ({ children }) => {
       const tokenRes = await fetch(url, options);
       if (!tokenRes.ok) throw new Error(`Error: ${tokenRes.statusText}`);
       const { token } = await tokenRes.json();
-      window.localStorage.setItem("token", token);
+      window.localStorage.setItem('token', token);
       await getUser(token);
-      navigate("/account");
+      navigate('/account');
     } catch (err) {
       setError(err.message);
       setLogin(false);
@@ -52,14 +52,14 @@ export const UserStorage = ({ children }) => {
 
   useEffect(() => {
     async function autoLogin() {
-      const token = window.localStorage.getItem("token");
+      const token = window.localStorage.getItem('token');
       if (token) {
         try {
           setError(null);
           setLoading(true);
           const { url, options } = TOKEN_VALIDATE_POST(token);
           const response = await fetch(url, options);
-          if (!response.ok) throw new Error("Token inválido");
+          if (!response.ok) throw new Error('Token inválido');
           await getUser(token);
         } catch (err) {
           userLogout();

f3fc86a644788e1c1c51ff5ffc42e3a81a315ae3
Gus <gusbdev@gmail.com>
2021-09-26 15:02:18 -0300

    Reseting password

--- src/Components/Login/LoginPasswordReset/LoginPasswordReset.js
+++ src/Components/Login/LoginPasswordReset/LoginPasswordReset.js
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Input from '../../../Components/Forms/Input/Input';
+import Button from '../../../Components/Forms/Button/Button';
+import Error from '../../../Helper/Error/Error';
+import useForm from '../../../Hooks/useForm';
+import useFetch from '../../../Hooks/useFetch';
+import { PASSWORD_RESET } from '../../../Services/api';
 
 const LoginPasswordReset = () => {
-  return <div>Login Password Reset</div>;
+  const [login, setLogin] = useState('');
+  const [key, setKey] = useState('');
+  const password = useForm();
+  const { error, loading, request } = useFetch();
+  const navigate = useNavigate();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (password.validate()) {
+      const { url, options } = PASSWORD_RESET({
+        login,
+        key,
+        password: password.value,
+      });
+      const { response } = await request(url, options);
+      if (response.ok) navigate('/login');
+    }
+  }
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const key = params.get('key');
+    const login = params.get('login');
+    if (key) setKey(key);
+    if (login) setLogin(login);
+  }, []);
+
+  return (
+    <div>
+      <h1 className="title">Resete a senha</h1>
+      <form onSubmit={handleSubmit}>
+        <Input
+          label="Nova senha"
+          type="password"
+          name="password"
+          {...password}
+        />
+        {loading ? (
+          <Button disabled>Resetando...</Button>
+        ) : (
+          <Button>Resetar</Button>
+        )}
+      </form>
+      <Error error={error} />
+    </div>
+  );
 };
 
 export default LoginPasswordReset;

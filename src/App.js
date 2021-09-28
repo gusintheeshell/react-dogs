098e09276ee01b2808ac8e4bbc1d237dbdc4ef0f
Gus <gusbdev@gmail.com>
2021-09-27 21:53:00 -0300

    Using autologin

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/Header';
@@ -10,8 +11,15 @@ import ProtectedRoute from './Helper/Route/ProtectedRoute';
 import Photo from './Components/Photo/Photo';
 import UserProfile from './Components/User/UserProfile/UserProfile';
 import NotFound from './Components/NotFound';
+import { autoLogin } from './Store/user';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(autoLogin());
+  }, [dispatch]);
+
   return (
     <div className="App">
       <BrowserRouter>

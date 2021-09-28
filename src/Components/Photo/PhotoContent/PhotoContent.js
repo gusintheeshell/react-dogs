8a2edccfdac1755be185c6346502fd609314c52e
Gus <gusbdev@gmail.com>
2021-09-27 22:07:27 -0300

    Remove unused import

--- src/Components/Photo/PhotoContent/PhotoContent.js
+++ src/Components/Photo/PhotoContent/PhotoContent.js
@@ -1,7 +1,6 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { UserContext } from '../../../Contexts/UserContext';
 import PhotoComments from '../PhotoComments/PhotoComments';
 import PhotoDelete from '../PhotoDelete/PhotoDelete';
 import Image from '../../../Helper/Image/Image';

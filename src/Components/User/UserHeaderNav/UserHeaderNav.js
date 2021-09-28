5cb5901593b4b2c114847bd8d1c7a3125e2b08ed
Gus <gusbdev@gmail.com>
2021-09-27 21:31:27 -0300

    Using logout from user reducer

--- src/Components/User/UserHeaderNav/UserHeaderNav.js
+++ src/Components/User/UserHeaderNav/UserHeaderNav.js
@@ -1,16 +1,17 @@
-import React, { useContext, useState } from "react";
-import { NavLink } from "react-router-dom";
-import { UserContext } from "../../../Contexts/UserContext";
-import { ReactComponent as Feed } from "../../../Assets/feed.svg";
-import { ReactComponent as Stats } from "../../../Assets/estatisticas.svg";
-import { ReactComponent as Post } from "../../../Assets/adicionar.svg";
-import { ReactComponent as Logout } from "../../../Assets/sair.svg";
-import useMedia from "../../../Hooks/useMedia";
-import styles from "./UserHeaderNav.module.css";
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { ReactComponent as Feed } from '../../../Assets/feed.svg';
+import { ReactComponent as Stats } from '../../../Assets/estatisticas.svg';
+import { ReactComponent as Post } from '../../../Assets/adicionar.svg';
+import { ReactComponent as Logout } from '../../../Assets/sair.svg';
+import useMedia from '../../../Hooks/useMedia';
+import styles from './UserHeaderNav.module.css';
+import { useDispatch } from 'react-redux';
+import { userLogout } from '../../../Store/user';
 
 const UserHeaderNav = () => {
-  const { userLogout } = useContext(UserContext);
-  const mobile = useMedia("(max-width: 40rem)");
+  const dispatch = useDispatch();
+  const mobile = useMedia('(max-width: 40rem)');
   const [mobileMenu, setMobileMenu] = useState(false);
 
   return (
@@ -31,19 +32,19 @@ const UserHeaderNav = () => {
       >
         <NavLink to="/account" end activeClassName={styles.active}>
           <Feed />
-          {mobile && "Minhas Fotos"}
+          {mobile && 'Minhas Fotos'}
         </NavLink>
         <NavLink to="/account/stats" activeClassName={styles.active}>
           <Stats />
-          {mobile && "Estatísticas"}
+          {mobile && 'Estatísticas'}
         </NavLink>
         <NavLink to="/account/post" activeClassName={styles.active}>
           <Post />
-          {mobile && "Adicionar Foto"}
+          {mobile && 'Adicionar Foto'}
         </NavLink>
-        <button onClick={userLogout}>
+        <button onClick={() => dispatch(userLogout())}>
           <Logout />
-          {mobile && "Sair"}
+          {mobile && 'Sair'}
         </button>
       </nav>
     </>
